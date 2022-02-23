7183ca609f15b1d0c2b769a11ed68878ebce76be
João Lucas <joao_pereira@gec.inatel.br>
2022-02-23 08:23:35 -0300

    added project base

---
+++ maven-e-versionamento/src/main/java/br/inatel/joao/Main.java
@@ -0,0 +1,40 @@
+package br.inatel.joao;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.nio.file.Path;
+import com.opencsv.bean.CsvToBeanBuilder;
+import br.inatel.joao.model.SellGames;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        try {
+            var fileReader = new FileReader(Path.of(System.getProperty("user.dir"), "vendas-games.csv").toString());
+
+            var listSellGames = new CsvToBeanBuilder<SellGames>(fileReader)
+                    .withType(SellGames.class)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .build().parse();
+
+            for (var sellGames : listSellGames) {
+                System.out.println("rank: " + sellGames.getRank());
+                System.out.println("name: " + sellGames.getName());
+                System.out.println("platform: " + sellGames.getPlatform());
+                System.out.println("year: " + sellGames.getYear());
+                System.out.println("genre: " + sellGames.getGenre());
+                System.out.println("publisher: " + sellGames.getPublisher());
+                System.out.println("na_sales: " + sellGames.getNa_sales());
+                System.out.println("eu_sales: " + sellGames.getEu_sales());
+                System.out.println("jp_sales: " + sellGames.getJp_sales());
+                System.out.println("other_sales: " + sellGames.getOther_sales());
+                System.out.println("global_sales: " + sellGames.getGlobal_sales());
+                System.out.println("-----------------------------------------------");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+
+        }
+
+    }
+}
---
+++ maven-e-versionamento/src/main/java/br/inatel/joao/model/SellGames.java
@@ -0,0 +1,18 @@
+package br.inatel.joao.model;
+
+import lombok.Data;
+
+@Data
+public class SellGames {
+    private int rank;
+    private String name;
+    private String platform;
+    private int year;
+    private String genre;
+    private String publisher;
+    private float na_sales;
+    private float eu_sales;
+    private float jp_sales;
+    private float other_sales;
+    private float global_sales;
+}
