c60d234833886f5c40998fa036ac382a50499cf5
João Lucas <joao_pereira@gec.inatel.br>
2022-02-23 11:04:51 -0300

    added csv filters

--- maven-e-versionamento/src/main/java/br/inatel/joao/Main.java
+++ maven-e-versionamento/src/main/java/br/inatel/joao/Main.java
@@ -3,13 +3,38 @@ package br.inatel.joao;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import com.opencsv.bean.CsvToBeanBuilder;
 import br.inatel.joao.model.SellGames;
 
 public class Main {
+    public static ArrayList<SellGames> filterByPlatforms(List<SellGames> csv, List<String> platforms) {
+        ArrayList<SellGames> filtered = new ArrayList<>();
+        for (SellGames game : csv) {
+            if (platforms.contains(game.getPlatform())) {
+                filtered.add(game);
+            }
+        }
+
+        return filtered;
+    }
+
+    public static ArrayList<SellGames> filterByPublishers(List<SellGames> csv, List<String> publishers) {
+        ArrayList<SellGames> filtered = new ArrayList<>();
+        for (SellGames game : csv) {
+            if (publishers.contains(game.getPublisher())) {
+                filtered.add(game);
+            }
+        }
+
+        return filtered;
+    }
 
     public static void main(String[] args) throws FileNotFoundException {
         try {
@@ -20,19 +45,17 @@ public class Main {
                     .withIgnoreLeadingWhiteSpace(true)
                     .build().parse();
 
-            for (var sellGames : listSellGames) {
-
+            for (var sellGames : filterByPlatforms(listSellGames, Arrays.asList("PS4", "PC"))) {
                 System.out.println("rank: " + sellGames.getRank());
                 System.out.println("name: " + sellGames.getName());
                 System.out.println("platform: " + sellGames.getPlatform());
-                System.out.println("year: " + sellGames.getYear());
-                System.out.println("genre: " + sellGames.getGenre());
+                System.out.println("-----------------------------------------------");
+            }
+
+            for (var sellGames : filterByPublishers(listSellGames, Arrays.asList("Sony Computer Entertainment", "Activision"))) {
+                System.out.println("rank: " + sellGames.getRank());
+                System.out.println("name: " + sellGames.getName());
                 System.out.println("publisher: " + sellGames.getPublisher());
-                System.out.println("na_sales: " + sellGames.getNa_sales());
-                System.out.println("eu_sales: " + sellGames.getEu_sales());
-                System.out.println("jp_sales: " + sellGames.getJp_sales());
-                System.out.println("other_sales: " + sellGames.getOther_sales());
-                System.out.println("global_sales: " + sellGames.getGlobal_sales());
                 System.out.println("-----------------------------------------------");
             }
         } catch (Exception e) {
